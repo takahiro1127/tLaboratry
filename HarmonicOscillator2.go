d7b2b8a279922674f89318dcefd59d2411d01ff6
Uehara takahiro <takahiro.steadtler@gmail.com>
2019-06-20 02:54:25 +0900

    setValuesAndCheckValues

--- HarmonicOscillator2.go
+++ HarmonicOscillator2.go
@@ -3,17 +3,19 @@ package main
 import (
 	"fmt"
 	"math"
-	"gonum.org/v1/plot"
-	"gonum.org/v1/plot/plotter"
-	"gonum.org/v1/plot/plotutil"
-	"gonum.org/v1/plot/vg"
+	// "gonum.org/v1/plot"
+	// "gonum.org/v1/plot/plotter"
+	// "gonum.org/v1/plot/plotutil"
+	// "gonum.org/v1/plot/vg"
 )
 
 type EquationInformation struct{
 	InitialX, InitialY, InitialdY, InfinityX, InfinityY, InfinitydY, TargetX, TargetY, L float64
 	SecondOrderDifferentalEquation func(x float64,y  float64, dy float64, p float64, l float64) float64
 	InitialEquation func(x float64, l float64) float64
+	InitialDifferentalEquation func(x float64, l float64) float64
 	InfinityEquation func(x float64, p float64) float64
+	InfinityDifferentalEquation func(x float64, p float64) float64
 }
 
 func main() {
@@ -24,8 +26,13 @@ func main() {
 		L: 1,
 		InitialEquation: defineInitialEquation,
 		InfinityEquation: defineInfinityEquation,
+		InitialDifferentalEquation: defineInitialDifferntalEquation,
+		InfinityDifferentalEquation: defineInfinityDifferentalEquation,
 		SecondOrderDifferentalEquation: defineSecondOrderDifferentalEquation,
 	}
+	// examination
+	targetEquation.SetInitialValues(1)
+	fmt.Printf("%+v\n", targetEquation)
 }
 
 func defineSecondOrderDifferentalEquation(x float64, y float64, dy float64, p float64, l float64) float64 {
@@ -37,7 +44,8 @@ func defineInitialEquation(x float64, l float64) float64 {
 }
 
 func defineInitialDifferntalEquation(x float64, l float64) float64 {
-	return (l + 1) * math.Pow(x, l)
+	return 1 //l = 1
+	//(l + 1) * math.Pow(x, l)
 }
 
 func defineInfinityEquation(x float64, p float64) float64 {
@@ -48,18 +56,9 @@ func defineInfinityDifferentalEquation(x float64, p float64) float64 {
 	return -1 * p * math.Pow(math.E, -p * x)
 }
 
-func (equationInformation *EquationInformation) SetInitialYvalue() {
+func (equationInformation *EquationInformation) SetInitialValues(p float64) {
 	equationInformation.InitialY = equationInformation.InitialEquation(equationInformation.InitialX, equationInformation.L)
-}
-
-func (equationInfotmation *EquationInformation) SetInitialdYValue() {
-	equationInformation.InitialY = equationInformation.InitialDifferntalEquation(equationInformation.InitialX, equationInformation.L)
-}
-
-func (equationInformation *EquationInformation) SetInfinityYvalue(p float64) {
+	equationInformation.InitialdY = equationInformation.InitialDifferentalEquation(equationInformation.InitialX, equationInformation.L)
 	equationInformation.InfinityY = equationInformation.InfinityEquation(equationInformation.InfinityX, p)
-}
-
-func (equationInformation *EquationInformation) SetInfinityYvalue(p float64) {
-	equationInformation.InfinityY = equationInformation.InfinityDiffrentalEquation(equationInformation.InfinityX, p)
+	equationInformation.InfinitydY = equationInformation.InfinityDifferentalEquation(equationInformation.InfinityX, p)
 }
